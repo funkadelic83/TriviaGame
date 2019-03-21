f9efdc451a900aaf8d08183718c8109938863fe0
funkadelic83 <mikedamanskis@gmail.com>
2019-03-21 09:06:32 -0700

    add timer, improve html/css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@ var correctAnswers = 0;
 var incorrectAnswers = 0;
 
 window.onload = function () {
-    $("#question").html("This is where the instructions would go.");
+    $("#question").html("How much do you know about politics? American politics is a circus, and this quiz will test how smart you are about our collective stupidity. Questions will range in difficulty from everyday knowledge to the obscure.");
     $("#answer").html("<button id='start'>Play Now</button>");
     $("#start").on("click", function () {
         question1();
@@ -36,41 +36,83 @@ function glowSelected() {
 }
 
 function question1() {
-    $("#question").html("<div class='question'>Add Question 1</div>");
-    $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
+    
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question2();
+        })
+    }}
+
+    $("#question").html('<div class="question">Which 2010 Senate candidate released a campaign ad that began with the line, "I&#8217;m not a witch. I&#8217;m nothing you&#8217;ve heard. I&#8217;m you."?</div>');
+    $("#answer").html("<div class='answer'><span id='answerOne'>Carly Fiorina</span></div><div class='answer'><span id='answerTwo'>Meg Whitman</span></div><div class='answer'><span id='answerThree'>Christine O'Donnell</span></div><div class='answer'><span id='answerFour'>Linda McMahon</span></div>");
     glowSelected();
 
-    $("#answerOne").on("click", function () {
+    $("#answerThree").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
-        $("#question").html("You have chosen wisely");
-        $("#answer").html("<button id='start'>Next</button>");
+        $("#question").html('Correct! Watch the ad here:');
+        $("#answer").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/uxJyPsmEask" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><button id="start">Next</button>');
         $("#start").on("click", function () {
             question2();
         })})
 
-    $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+    $("#answerTwo, #answerOne, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
-        $("#question").html("You have chosen poorly");
-        $("#answer").html("<button id='start'>Next</button>");;
+        $("#question").html('Wrong! But none of us are perfect. It was Christine O&#8217;Donnell. Watch the ad here:');
+        $("#answer").html('<iframe width="560" height="315" src="https://www.youtube.com/embed/uxJyPsmEask" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><button id="start">Next</button>');
         $("#start").on("click", function () {
             question2();
         })})
 }
 
 function question2() {
-    $("#question").html("<div class='question'>Add Question 2</div>");
-    $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question3();
+        })
+    }}
+    $("#question").html("<div class='question'>Which US President made the Bush tax cuts permanent?</div>");
+    $("#answer").html("<div class='answer'><span id='answerOne'>George W. Bush</span></div><div class='answer'><span id='answerTwo'>Barack Obama</span></div><div class='answer'><span id='answerThree'>The Bush tax cuts have expired.</span></div><div class='answer'><span id='answerFour'>Donald J. Trump</span></div>");
     glowSelected();
 
-    $("#answerOne").on("click", function () {
+    $("#answerTwo").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
-        $("#question").html("You have chosen wisely");
+        $("#question").html("Correct! Before the Bush tax cuts, the top marginal tax rate was 39.6 percent. After the cuts, the top rate was 35%. The Bush tax cuts were set to expire in 2010, Barack Obama made the Bush Tax cuts permanent (after a temporary, 2-year extension).");
         $("#answer").html("<button id='start'>Next</button>");
         $("#start").on("click", function () {
             question3();
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Next</button>");;
@@ -80,11 +122,29 @@ function question2() {
 }
 
 function question3() {
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question4();
+        })
+    }}
     $("#question").html("<div class='question'>Add Question 3</div>");
     $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
     glowSelected();
 
     $("#answerOne").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
         $("#question").html("You have chosen wisely");
         $("#answer").html("<button id='start'>Next</button>");
@@ -93,6 +153,8 @@ function question3() {
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Next</button>");;
@@ -102,11 +164,29 @@ function question3() {
 }
 
 function question4() {
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question5();
+        })
+    }}
     $("#question").html("<div class='question'>Add Question 4</div>");
     $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
     glowSelected();
 
     $("#answerOne").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
         $("#question").html("You have chosen wisely");
         $("#answer").html("<button id='start'>Next</button>");
@@ -115,6 +195,8 @@ function question4() {
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Next</button>");;
@@ -123,11 +205,29 @@ function question4() {
         })})
 }
 function question5() {
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question6();
+        })
+    }}
     $("#question").html("<div class='question'>Add Question 5</div>");
     $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
     glowSelected();
 
     $("#answerOne").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
         $("#question").html("You have chosen wisely");
         $("#answer").html("<button id='start'>Next</button>");
@@ -136,6 +236,8 @@ function question5() {
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Next</button>");;
@@ -145,11 +247,29 @@ function question5() {
 }
 
 function question6() {
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question7();
+        })
+    }}
     $("#question").html("<div class='question'>Add Question 6</div>");
     $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
     glowSelected();
 
     $("#answerOne").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
         $("#question").html("You have chosen wisely");
         $("#answer").html("<button id='start'>Next</button>");
@@ -158,6 +278,8 @@ function question6() {
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Next</button>");;
@@ -167,11 +289,29 @@ function question6() {
 }
 
 function question7() {
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question8();
+        })
+    }}
     $("#question").html("<div class='question'>Add Question 7</div>");
     $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
     glowSelected();
 
     $("#answerOne").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
         $("#question").html("You have chosen wisely");
         $("#answer").html("<button id='start'>Next</button>");
@@ -180,6 +320,8 @@ function question7() {
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Next</button>");;
@@ -189,11 +331,29 @@ function question7() {
 }
 
 function question8() {
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question9();
+        })
+    }}
     $("#question").html("<div class='question'>Add Question 8</div>");
     $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
     glowSelected();
 
     $("#answerOne").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
         $("#question").html("You have chosen wisely");
         $("#answer").html("<button id='start'>Next</button>");
@@ -202,6 +362,8 @@ function question8() {
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Next</button>");;
@@ -211,11 +373,29 @@ function question8() {
 }
 
 function question9() {
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">Next</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                question10();
+        })
+    }}
     $("#question").html("<div class='question'>Add Question 9</div>");
     $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
     glowSelected();
 
     $("#answerOne").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
         $("#question").html("You have chosen wisely");
         $("#answer").html("<button id='start'>Next</button>");
@@ -224,6 +404,8 @@ function question9() {
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Next</button>");;
@@ -233,11 +415,29 @@ function question9() {
 }
 
 function question10() {
+    var timeLeft = 10;
+    $("#timer").html("Time remaining: " + timeLeft);
+    var questionTimer = setInterval(questionTimer, 1000);
+    function questionTimer() {
+        timeLeft--;
+        $("#timer").html("Time remaining: " + timeLeft);
+        if (timeLeft <= 0) {
+            clearInterval(questionTimer);
+            incorrectAnswers++;
+            $("#question").html('<div class="question">Time is up!</div>');
+            $("#answer").html('<button id="start">View Results</button>');
+            $("#timer").html('');
+            $("#start").on("click", function () {
+                results();
+        })
+    }}
     $("#question").html("<div class='question'>Add Question 10</div>");
     $("#answer").html("<div class='answer'><span id='answerOne'>AnswerOne</span></div><div class='answer'><span id='answerTwo'>AnswerTwo</span></div><div class='answer'><span id='answerThree'>AnswerThree</span></div><div class='answer'><span id='answerFour'>AnswerFour</span></div>");
     glowSelected();
 
     $("#answerOne").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         correctAnswers++;
         $("#question").html("You have chosen wisely");
         $("#answer").html("<button id='start'>Next</button>");
@@ -246,6 +446,8 @@ function question10() {
         })})
 
     $("#answerTwo, #answerThree, #answerFour").on("click", function () {
+        clearInterval(questionTimer);
+        $("#timer").html('');
         incorrectAnswers++;
         $("#question").html("You have chosen poorly");
         $("#answer").html("<button id='start'>Results</button>");;
